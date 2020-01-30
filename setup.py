c462b4632c1928437ac5978b5c077f9d1c4f3923
mfellows <mfellows@nrcan.gc.ca>
2020-01-30 11:03:20 -0800

    Added missing utm dependency, fixed pysal version.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     ],
     keywords="moja.global",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
-    install_requires=["numpy", "matplotlib", "seaborn", "imageio", "imageio-ffmpeg", "pillow", "geopy", "pysal==1.14.4"],
+    install_requires=["numpy", "matplotlib", "seaborn", "imageio", "imageio-ffmpeg", "pillow", "geopy", "pysal<=1.15.0", "utm"],
     extras_require={},
     package_data={},
     data_files=[],
